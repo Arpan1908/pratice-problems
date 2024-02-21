fb9b1920e10859f4532be54258d8e37584c680a8
ARPAN CHOWDHURY <77331658+Arpan1908@users.noreply.github.com>
2024-02-21 16:33:02 +0530

    Create product of digits.java

---
+++ product of digits.java
@@ -0,0 +1,16 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+
+class HelloWorld {
+    public static void main(String[] args) {
+        int ans = sum(1234);
+        System.out.println(ans);
+    }
+    
+    static int sum(int n){
+        if(n%10==n){
+            return n;
+        }
+        return (n%10) * sum(n/10);
+    }
+}

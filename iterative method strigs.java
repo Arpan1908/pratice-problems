e7550dc0aceb111f623148ca5b4d7d0528530e39
ARPAN CHOWDHURY <77331658+Arpan1908@users.noreply.github.com>
2024-03-03 16:42:40 +0530

    Create iterative method strigs.java

---
+++ iterative method strigs.java
@@ -0,0 +1,24 @@
+public class Main
+{
+	public static void main(String[] args) {
+		int[] arr = {1,2,3};
+		List<List<Integer>> ans = subseq(arr);
+		for(List<Integer> list:ans){
+		    System.out.println(list);
+		}
+		
+	}
+	
+	static List<List<Integer>> subseq(int[] arr){
+	    List<List<Integer>> outer = new ArrayList<>();
+	    for(int num:outer){
+	        int n = outer.size();
+	        for(int i=0;i<n;i++){
+	            List<Integer> internal = new ArrayList<>(outer.get(i));
+	            internal.add(num);
+	            outer.add(internal);
+	        }
+	    }
+	    return outer;
+	}
+}
